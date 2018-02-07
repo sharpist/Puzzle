87fb09958ea117e4334ebd2e593ae0d8bfb0b58c
Alexander Usov <alexusov@photographer.net>
2018-02-07 22:18:13 +0300

    Puzzle

---
+++ Factorial/Factorial.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Threading;
+using System.Numerics;
+using System.Threading.Tasks;
+
+class Program
+{
+    static void Main()
+    {
+        long n = 20; // найти факториал натурального числа n
+
+
+        Task.Run(() => { myThread(n); })
+            .ConfigureAwait(false).GetAwaiter().GetResult();
+
+        Console.WriteLine($"Factorial of {n} equals {result}");
+        // Factorial of 20 equals 2432902008176640000
+    }
+
+
+    static void myThread(long n)
+    {
+        var thread = new Thread(() => { result = Factorial(n); });
+        thread.Start(); thread.Join(); // запустить и ждать завершения 
+    }
+
+    static BigInteger Factorial(long n)
+    {
+        BigInteger result = n; int factor = 0;
+        while (!(++factor >= n)) result *= factor;
+
+        return result!=0?result:1;
+    }
+    static BigInteger result;
+}
---
+++ Factorial/README.md
@@ -0,0 +1,5 @@
+## the factorial of a non-negative integer n
+
+5! = 1 * 2 * 3 * 4 * 5 = 120
+
+* the value of 0! is 1, according to the convention for an empty product
