4a735a9ead3faca22ee41880dbbe621a5b836d26
Alexander Usov <alexusov@photographer.net>
2018-08-22 11:11:20 +0300

    C_Sharp

---
+++ Greatest Common Divisor/Greatest Common Divisor.cs
@@ -0,0 +1,15 @@
+﻿class Program
+{
+    static void Main() =>
+        System.Console.WriteLine(GCD(8, 16)); // 8
+
+
+    // рекурсивный метод, вычисляющий наибольший
+    // общий делитель двух целых чисел
+    static int GCD(int a, int b)
+    {
+        if (b == 0) return a;
+
+        return GCD(b, a % b);
+    }
+}
