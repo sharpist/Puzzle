806b895e95edada4e5146a666c5ff2491c4d8ca2
Alexander Usov <28162428+sharpist@users.noreply.github.com>
2018-10-02 02:50:49 +0300

    C_Sharp

--- Pscl Triangle/PsclTriangle2.cs
+++ Pscl Triangle/PsclTriangle2.cs
@@ -58,12 +58,18 @@ struct PsclTriangle
         return triangle;
     }
 
-    public static ushort[] FibDiagonal(ushort[][] triangle, int row)
+    public static ushort[] FibDiagonal(ushort[][] triangle, byte index)
     {
-        var column = 0;
-        var diagonal = new ushort[(row + 2) / 2];
-        while (row >= column)
-            diagonal[column] = triangle[row--][column++];
+        var capacity = ((index + 2) / 2);
+        var diagonal = new ushort[capacity];
+
+        for ( ; capacity > 0; capacity--)
+        {
+            var row    = index - (capacity - 1);
+            var column = (capacity - 1);
+
+            diagonal[column] = triangle[row][column];
+        }
         return diagonal;
     }
 }
